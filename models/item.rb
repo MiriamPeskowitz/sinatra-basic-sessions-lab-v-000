ab62e08d7bddbafed028300732e9934836264ed7
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-21 02:02:40 +0000

    automatically backed up by learn

--- models/item.rb
+++ models/item.rb
@@ -1,8 +1,8 @@
 class Item
-  attr_accessor :item
+  attr_accessor :name
 
-    def initialize(item)
-      @item = item
+    def initialize(name)
+      @name = name
     end
 
     #need to collect more?
